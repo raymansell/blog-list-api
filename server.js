f30ce9f8a2e4a7685f80a35fa4cd51624e940fac
raymansell <61888240+raymansell@users.noreply.github.com>
2021-02-07 19:25:02 -0500

    initial server

---
+++ server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config/.env' });
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = process.env.PORT || 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
