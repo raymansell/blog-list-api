d111f16c5842c0368323c9c2317cbdf415cf81cd
raymansell <61888240+raymansell@users.noreply.github.com>
2021-02-10 18:56:18 -0500

    put and delete routes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,17 +3,55 @@ const Blog = require('../models/blog');
 
 router
   .route('/')
-  .get(async (req, res) => {
-    const blogs = await Blog.find({});
-    res.json(blogs);
+  .get(async (req, res, next) => {
+    try {
+      const blogs = await Blog.find({});
+      res.json(blogs);
+    } catch (error) {
+      next(error);
+    }
   })
-  .post(async (req, res) => {
+  .post(async (req, res, next) => {
     if (!req.body.title && !req.body.url) {
       return res.status(400).end(); // return prevents further execution of function
     }
-    const blog = new Blog(req.body);
-    const result = await blog.save();
-    res.status(201).json(result);
+    try {
+      const blog = new Blog(req.body);
+      const result = await blog.save();
+      res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+router
+  .route('/:id')
+  .get(async (req, res, next) => {
+    try {
+      const blog = await Blog.findById(req.params.id);
+      res.json(blog);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .put(async (req, res, next) => {
+    const { id, ...blog } = req.body;
+    try {
+      const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+        new: true,
+      });
+      res.json(updatedBlog);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      const blog = await Blog.findByIdAndDelete();
+      res.status(204).end();
+    } catch (error) {
+      next(error);
+    }
   });
 
 module.exports = router;
